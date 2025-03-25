63589fdd8306522e70c6e0732c6bf31d82f083d2
Makhmudjonov <makhmudjonov14@gmail.com>
2025-03-26 01:59:20 +0500

    fix

--- config/settings.py
+++ config/settings.py
@@ -67,6 +67,8 @@ CORS_ALLOWED_ORIGINS = [
     "http://localhost:5173",  # Vite yoki boshqa frontend ishlayotgan port
     "http://127.0.0.1:5173",
     "http://localhost:3000",
+    "https://cert.tma.uz",
+    "https://mforms.uz"
 ]
 
 # Agar barcha domenlarni ruxsat bermoqchi bo‘lsangiz:
