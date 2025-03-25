edd7812cf3d85177a2cdb5b6625e7e129771e60e
Makhmudjonov <makhmudjonov14@gmail.com>
2025-03-26 01:57:56 +0500

    fix

--- config/settings.py
+++ config/settings.py
@@ -66,6 +66,7 @@ MIDDLEWARE = [
 CORS_ALLOWED_ORIGINS = [
     "http://localhost:5173",  # Vite yoki boshqa frontend ishlayotgan port
     "http://127.0.0.1:5173",
+    "http://localhost:3000",
 ]
 
 # Agar barcha domenlarni ruxsat bermoqchi bo‘lsangiz:
